77765be48e7bba555618bf449e1031d8b3404890
chaxmix <41344298+chaxmix@users.noreply.github.com>
2023-04-30 23:13:20 -0400

    changes

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-// Fetch data from USGS earthquake API
-async function fetchData() {
-    const response = await fetch('https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2023-01-01&endtime=2023-04-23');
-    const data = await response.json();
-    return data.features;
-  }
-  
-  // Process the fetched data using array methods
-  async function processData() {
-    const data = await fetchData();
-    const processedData = data
-      .filter(feature => feature.properties.mag >= 5)
-      .map(feature => ({
-        magnitude: feature.properties.mag,
-        location: feature.properties.place,
-        coordinates: feature.geometry.coordinates,
-      }))
-      .sort((a, b) => b.magnitude - a.magnitude);
-    return processedData;
-  }
-  
-  // Use processed data in a visualization library
-  async function displayData() {
-    const processedData = await processData();
-    console.log(processedData);
-  }
-  // IDK how to make this work with the html to make it visible 
-  
-  displayData();
+$(document).ready(function() {
+  const url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2023-01-01&endtime=2023-04-30&minmagnitude=2.5&limit=500";
+
+  $.get(url, function(response) {
+    const earthquakes = response.features;
+
+    const markers = [];
+    const map = L.map('map').setView([38.98, -76.93], 13);
+
+    for (let i = 0; i < earthquakes.length; i++) {
+      const earthquake = earthquakes[i];
+      const coordinates = earthquake.geometry.coordinates;
+      const magnitude = earthquake.properties.mag;
+
+      const marker = L.circleMarker([coordinates[1], coordinates[0]], {
+        radius: magnitude * 5,
+        fillColor: "#f00",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+      });
+
+      markers.push(marker);
+    }
+
+    const earthquakeLayer = L.layerGroup(markers);
+    earthquakeLayer.addTo(map);
+  });
+});
