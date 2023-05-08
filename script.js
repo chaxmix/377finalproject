f8c03c37d24ca8e41094aa0f8ea995afc52336e2
chaxmix <41344298+chaxmix@users.noreply.github.com>
2023-05-07 23:33:28 -0400

    js html changes

--- script.js
+++ script.js
@@ -10,34 +10,53 @@ $(document).ready(function() {
     return data ? JSON.parse(data) : null;
   }
 
+  function fetchData() {
+    const url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2023-01-01&endtime=2023-04-30&minmagnitude=2.5&limit=500";
 
-$(document).ready(function() {
-  const url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2023-01-01&endtime=2023-04-30&minmagnitude=2.5&limit=500";
+    $.get(url, function(response) {
+      const earthquakes = response.features;
+      saveDataToLocalStorage(earthquakes);
+      renderMarkers(earthquakes);
+    });
+  }
 
-  $.get(url, function(response) {
-    const earthquakes = response.features;
+  function renderMarkers(earthquakes) {
+      const markers = [];
+      const map = L.map('map').setView([38.98, -76.93], 13);
 
-    const markers = [];
-    const map = L.map('map').setView([38.98, -76.93], 13);
+      for (let i = 0; i < earthquakes.length; i++) {
+        const earthquake = earthquakes[i];
+        const coordinates = earthquake.geometry.coordinates;
+        const magnitude = earthquake.properties.mag;
 
-    for (let i = 0; i < earthquakes.length; i++) {
-      const earthquake = earthquakes[i];
-      const coordinates = earthquake.geometry.coordinates;
-      const magnitude = earthquake.properties.mag;
+        const marker = L.circleMarker([coordinates[1], coordinates[0]], {
+          radius: magnitude * 5,
+          fillColor: "#f00",
+          color: "#000",
+          weight: 1,
+          opacity: 1,
+          fillOpacity: 0.8
+        });
 
-      const marker = L.circleMarker([coordinates[1], coordinates[0]], {
-        radius: magnitude * 5,
-        fillColor: "#f00",
-        color: "#000",
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 0.8
-      });
+        markers.push(marker);
+      }
 
-      markers.push(marker);
+      const earthquakeLayer = L.layerGroup(markers);
+      earthquakeLayer.addTo(map);
     }
-
-    const earthquakeLayer = L.layerGroup(markers);
-    earthquakeLayer.addTo(map);
+    
+    function init() {
+      const savedData = getDataFromLocalStorage();
+      if (savedData) {
+        renderMarkers(savedData);
+      } else {
+        fetchData();
+      }
+    }
+  
+    init();
+  
+    $('#refresh-btn').click(function() {
+      fetchData();
+    });
   });
-});
